dc67c0e399e2454d27f976c3d18e77ef59fb42b8
Ariel Sebastián Gatti <arielgatti444@gmail.com>
2022-08-20 09:19:58 -0300

    Modificacion 1
    Esta es la primera modificacion generada para el proyecto

--- Aplicacion de Prueba/Prueba1CAI/Program.cs
+++ Aplicacion de Prueba/Prueba1CAI/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Probando el control de cambios en C#");
+Console.WriteLine("Hola");
+Console.WriteLine("a");
+Console.WriteLine("todos!!");
